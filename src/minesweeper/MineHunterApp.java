75d4ba2afcd01238d67a536c30fa44362f2e9cb1
Julia <dart1ju@gmail.com>
2018-08-02 13:07:09 -0400

    first commit

---
+++ src/minesweeper/MineHunterApp.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package minesweeper;
+
+/**
+ * @author Julia Dart
+ * @author Matthew Rhadigan
+ * @author Cheo Rowzee
+ * @author Jacob Quickert
+ * 
+ *
+ */
+public class MineHunterApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Welcome to the MineHunter game!");
+
+	}
+
+}
