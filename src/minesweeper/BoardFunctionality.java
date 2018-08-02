c3119ddcc4f5d70016642a29cb34bcca48dc292b
jacob <jacob.quickert@gmail.com>
2018-08-02 15:30:25 -0400

    added for loop for test printing

--- src/minesweeper/BoardFunctionality.java
+++ src/minesweeper/BoardFunctionality.java
@@ -14,7 +14,20 @@ public class BoardFunctionality {
 			{'*','*','2','-','2','*','3','1','-'},
 			{'3','*','2','-','1','1','1','-','-'}
 			};
-private static char[][]userBoard = new char[9][9];
+	private static char[][]userBoard = new char[9][9];
 
+	public static void testPrint() {
+		// for test printing
+		int rows = 9;
+		int columns = 9;
+				
+		for(int i = 0; i<rows; i++){
+			for(int j = 0; j<columns; j++){
+				System.out.format("%4s", answerBoard[i][j]);
+			}
+			System.out.println();
+		}
+		System.out.println(Arrays.deepToString(answerBoard));
+	}
 
 }
