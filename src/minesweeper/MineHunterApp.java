85d75134728923c3126b66a280d9d9d2bf373339
Cheo <cheorowzee83@gmail.com>
2018-08-03 16:55:02 -0400

    revised main2

--- src/minesweeper/MineHunterApp.java
+++ src/minesweeper/MineHunterApp.java
@@ -1,6 +1,6 @@
-  /**
- * 
- */
+/**
+* 
+*/
 package minesweeper;
 
 import java.util.Scanner;
@@ -24,71 +24,50 @@ public class MineHunterApp {
 		String inNum;
 		String isLastFlag;
 		String check = null;
-		String answer = null;
+
 		int size;
-		
-		
+
 		System.out.println("Welcome to the MineHunter game!");
-		
-		
+
 		System.out.println("Please enter the level of difficulty. (easy, intermediate, difficult) ");
 		String sizeInput = scan.nextLine();
-		
-		
-		// Outputting board from Functionality class and generateBoard methods based on level of difficulty selected
-        if (sizeInput.equalsIgnoreCase("easy")) {
-            BoardFunctionality.generateEasyBoard();
-        } else if (sizeInput.equalsIgnoreCase("intermediate")) {
-            BoardFunctionality.generateMediumBoard();
-        } else if (sizeInput.equalsIgnoreCase("difficult")) {
-            BoardFunctionality.generateHardBoard();
-        }
-        //Functionality2.testPrint(Functionality2.generateEasyBoard());
-		
-		
-		
+
+		// Outputting board from Functionality class and generateBoard methods based on
+		// level of difficulty selected
+		if (sizeInput.equalsIgnoreCase("easy")) {
+			BoardFunctionality.generateEasyBoard();
+		} else if (sizeInput.equalsIgnoreCase("intermediate")) {
+			BoardFunctionality.generateMediumBoard();
+		} else if (sizeInput.equalsIgnoreCase("difficult")) {
+			BoardFunctionality.generateHardBoard();
+		}
+		// Functionality2.testPrint(Functionality2.generateEasyBoard());
+
 		// Outprint of board
-		do {		
-		System.out.println("Please choose a square! (x coordinate)");
-		int userX = scan.nextInt();
-		
-		System.out.println("Please choose a square! (y coordinate)");
-		int userY = scan.nextInt();
-		
-		System.out.println("What would you like to do with this spot? (click, flag, unflag)");
-		String userAction = scan.nextLine();
-		
-		// reference check cell to check user input 
-		
-		if(check.equalsIgnoreCase("bomb") ) {
-			System.out.println("You Lose!");
-			// display answer board
+		do {
+			System.out.println("Please choose a square! (x coordinate)");
+			int userX = scan.nextInt();
+
+			System.out.println("Please choose a square! (y coordinate)");
+			int userY = scan.nextInt();
+			scan.nextLine();
+
+			System.out.println("What would you like to do with this spot? (click, flag, unflag)");
+			String userAction = scan.nextLine();
+
+			// new updated board
+			// until they either win or lose
+			check = scan.next();
+			//if user doesn't lose or win program loops
+		} while (!check.equalsIgnoreCase("Win") || !check.equalsIgnoreCase("Lose"));
+
+		if (check.equalsIgnoreCase("win")) {
+			System.out.println("You Win!!!");
+		} else if (check.equalsIgnoreCase("lose")) {
+			System.out.println("You Lose!!!");
 		}
-		else if(check.equalsIgnoreCase("number")) {
-			// update board method
-			// uncover AllToNums
-			// redisplaying new Board
-		}
-		else if(check.equals("number")) {
-			// uncoverNum
-			// redisplay board
-		}
-		else if(check.equalsIgnoreCase("last flag")) {
-			System.out.println("You Win!");
-			// display answer
-		}
-		}while(!answer.equalsIgnoreCase("Win") || !answer.equalsIgnoreCase("Lose"));
-		
-		
-		
-		
-		
-		
-		
-		
+
+		scan.close();
 	}
 
 }
-
-
- 
